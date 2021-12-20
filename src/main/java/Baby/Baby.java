7a1252c55e58e652dc64fa5627d6cc604047f82e
ryanling777 <ryannicling@gmail.com>
2021-12-20 16:30:22 +0000

    added baby package and class

---
+++ src/main/java/Baby/Baby.java
@@ -0,0 +1,16 @@
+package Baby;
+import java.util.HashMap;
+
+//needs function to take data from excel sheet continuously
+//set and get methods
+public class Baby {
+    private String ID;
+    private HashMap< String, String> noteTimestamp = new HashMap<String,String>();
+    private HashMap< String, String> prickTimestamp = new HashMap<String,String>();
+    private HashMap< String, String> sweatTimestamp = new HashMap<String,String>();
+
+
+
+
+
+}
