6e63988aaeee69bfb73a090b18ecbeacc1e2dea7
ryanling777 <ryannicling@gmail.com>
2021-12-29 15:09:47 +0000

    returns a 1 element list when a single baby is requested by id

--- src/main/java/com/project/neo/Baby/BabyService.java
+++ src/main/java/com/project/neo/Baby/BabyService.java
@@ -138,8 +138,8 @@ public class BabyService {
 
     //takes the id of the selected baby and retrieves sweat info from the database
     public void UpdateSweatLevels (int i) {
-        //String file = "C:\\Users\\65978\\OneDrive - Imperial College London\\Desktop\\"+ i + ".csv"; // -< This is the path where the csv is saved.
-        String file = "C:\\Users\\65978\\OneDrive - Imperial College London\\Desktop\\test3.csv";
+        String file = "C:\\Users\\65978\\OneDrive - Imperial College London\\Desktop\\"+ i + ".csv"; // -< This is the path where the csv is saved.
+        //String file = "C:\\Users\\65978\\OneDrive - Imperial College London\\Desktop\\test3.csv";
         BufferedReader reader1 = null;
         String line = "";
 
