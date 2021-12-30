7cb7ceb8a0a8efb215fcae74e1633b864fcb7a39
youshsri <72707236+youshsri@users.noreply.github.com>
2021-12-30 22:02:03 +0530

    Added skeleton for BabyServiceTest, applying JUnit 5

---
+++ src/test/java/com/project/neo/Baby/BabyServiceTest.java
@@ -0,0 +1,48 @@
+package com.project.neo.Baby;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BabyServiceTest {
+
+    @Test
+    void returnBaby() {
+    }
+
+    @Test
+    void addNewBaby() {
+    }
+
+    @Test
+    void deleteBaby() {
+    }
+
+    @Test
+    void add_NoteTimeStamp() {
+    }
+
+    @Test
+    void add_PrickTimeStamp() {
+    }
+
+    @Test
+    void add_SweatTimeStamp() {
+    }
+
+    @Test
+    void returnSingleBaby() {
+    }
+
+    @Test
+    void updateSweatLevels() {
+    }
+
+    @Test
+    void addCalibration() {
+    }
+
+    @Test
+    void addDelay() {
+    }
+}
