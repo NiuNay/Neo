aa79a5fe28e3416539823ca67cfc25497fe8e516
youshsri <72707236+youshsri@users.noreply.github.com>
2022-01-08 12:34:45 +0000

    Added authorization filter for incoming login requests

--- src/main/java/com/project/neo/Security/Filters/CustomAuthenticationFilter.java
+++ src/main/java/com/project/neo/Security/Filters/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.project.neo.Security.Filters;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -55,7 +56,10 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
-        response.setHeader("access_token", accessToken);
-        response.setHeader("refresh_token", refreshToken);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", accessToken);
+        tokens.put("refresh_token", refreshToken);
+        response.setContentType("application/json");
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
