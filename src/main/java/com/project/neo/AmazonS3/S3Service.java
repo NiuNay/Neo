f8650bc70355e1384925643711ead2be60cef479
ryanling777 <ryannicling@gmail.com>
2022-01-09 12:10:21 +0000

    Creating a new branch for unit testing backend before subsequent deployments

--- src/main/java/com/project/neo/AmazonS3/S3Service.java
+++ src/main/java/com/project/neo/AmazonS3/S3Service.java
@@ -23,10 +23,13 @@ public class S3Service {
     private AmazonS3 s3client;
 
 
-    private String accessKeyId = System.getenv("KEY_ID");
+    //private String accessKeyId = System.getenv("KEY_ID");
+    @Value("${cloud.aws.credentials.access-key}$")
+    private String accessKeyId;
 
-
-    private String secretKey = System.getenv("SECRET_KEY");
+    //private String secretKey = System.getenv("SECRET_KEY");
+    @Value("${cloud.aws.credentials.secret-key}$")
+    private String secretKey;
 
     @Value("${s3.region}")
     private String region;
